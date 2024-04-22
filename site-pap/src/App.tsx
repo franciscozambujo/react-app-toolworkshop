e8a260016ae5b88934389803f5e019a269e33b00
XiCO1544 <102925550+XiCO144@users.noreply.github.com>
2024-04-22 19:49:54 +0100

    s

--- site-pap/src/App.tsx
+++ site-pap/src/App.tsx
@@ -2,24 +2,7 @@ import { headerMenuNav } from "./components/headerMenu";
 import { carouselMain } from "./components/carousel-section";
 import { mainPage } from "./components/main-page";
 import { Footer } from "./components/footer";
-<<<<<<< HEAD
 
-=======
->>>>>>> 9ee4e119bc73b098787eff74fd7ea6a2c01b19af
-const footerLinks = [
-  {
-    label: "Sobre Nós",
-    href: "/about",
-  },
-  {
-    label: "Contacto",
-    href: "/about",
-  },
-  {
-    label: "Agendar Revisão",
-    href: "/",
-  },
-];
 export function App() {
   return (
     <div>
