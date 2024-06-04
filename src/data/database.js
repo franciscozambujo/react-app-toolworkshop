296ace078b09a5f2e0d7060e448d08baab1a5c61
XiCO1544 <102925550+XiCO144@users.noreply.github.com>
2024-06-04 19:03:37 +0100

    deploy #5

--- src/data/database.js
+++ src/data/database.js
@@ -1,9 +1,9 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-  host: '144.64.182.184',
-  user : 'site',
-  password : 'Aspirina2019@',
+  host: 'localhost',
+  user : 'root',
+  password : '',
   database: 'db_oficina'
 }).promise();
 
