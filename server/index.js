9fe14be3d240750f687ba186836bb877821d6788
XiCO1544 <102925550+XiCO144@users.noreply.github.com>
2024-06-09 17:00:05 +0100

    testing routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require ('express');
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 app.use('/', (req,res) => {
     res.send("Hello World.");
 })
