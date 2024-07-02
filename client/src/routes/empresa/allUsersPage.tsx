950ef08977d5433d7bb74401853eaac81676cdb6
Francisco Zambujo <102925550+XiCO144@users.noreply.github.com>
2024-07-02 09:00:37 +0100

    .

--- client/src/routes/empresa/allUsersPage.tsx
+++ client/src/routes/empresa/allUsersPage.tsx
@@ -7,7 +7,7 @@ export function AllUsersPage() {
   return (
     <div className="h-screen flex flex-col">
       <Redirect/>
-      <div className="flex">
+      <div className="flex h-full">
           <aside>
               <SideBarEnterprise/>
           </aside>
